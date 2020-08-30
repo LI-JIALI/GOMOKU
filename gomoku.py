0009dd3fd4332dc78f8e604bac8a1d7fee2acdf5
LI-JIALI <70353365+LI-JIALI@users.noreply.github.com>
2020-08-30 22:58:22 +0200

    GOMOKU
    Code python en gomoku

---
+++ gomoku.py
@@ -0,0 +1,12 @@
+
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+import tkinter.messagebox
+#tkinter,messagebox fournit un assortiment de fenêtres de dialogue pour des tâches simples
+
+BLACK = True    #Ici on juge qui jouera aux échecs en premier
+END = False     #Ici on juge si le jeu est terminé
+
+
+length = 15     #L'Échiquier de taille 15x15
